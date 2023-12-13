44f70b627c3a06c1ba07625ab67541dcf307ac99
naveen_msi <naveen.aifanatic@gmail.com>
2023-12-13 00:36:56 -0600

    Updated site config

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Naveen Chatlapalli",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Solution Architect",
+    bio: "I develop everything using AI.",
+    email: "naveen.aifanatic@gmail.com",
+    linkedin: "nchatlapalli",
+    github: "ai-fanatic",
+    instagram: "navaifanatic",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `AIFanatic Blog`,
+      href: "https://github.com/ai-fanatic/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "AIFanatic Blog",
+    description: "welcome to AIFanatic Blog!",
   },
 
   // CONFIG configration (required)
@@ -47,9 +47,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "ai-fanatic/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
